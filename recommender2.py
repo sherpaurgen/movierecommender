7fc38b0f189c8402840d02b2bee3efe69440a138
chingchong <norbuurgen@gmail.com>
2016-08-31 13:30:41 +0545

    Create recommender2.py

---
+++ recommender2.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+r_cols=['user_id','movie_id','rating']
+ratings=pd.read_csv('/home/urgen/Downloads/ml-latest-small/ratings.csv',sep=',',names=r_cols,usecols=[0,1,2])
+m_cols=['movie_id','title']
+movies=pd.read_csv('/home/urgen/Downloads/ml-latest-small/movies.csv',sep=',',names=m_cols,usecols=[0,1])
+ratings=pd.merge(movies,ratings)
+ratings.head()
+movieRatings = ratings.pivot_table(index=["user_id"],columns="title",values="rating")
+movieRatings.head()
+toystoryrating=movieRatings['Austin Powers: The Spy Who Shagged Me (1999)']
+toystoryrating.head()
+similarMovies=movieRatings.corrwith(toystoryrating)
+# http://tungwaiyip.info/2012/Collaborative%20Filtering.html
+# finding the similarity score between the movies..score close to 1 means their tastes are very similar
+similarMovies=similarMovies.dropna()
+df = pd.DataFrame(similarMovies)
+#df
+#similarMovies.order(ascending=False)
+movieStats=ratings.groupby('title').agg({'rating': [np.size,np.mean]})
+movieStats.head()
+similarMovies.sort_values(ascending=False)
+movieStats.head()
+#taking movies rated by more than 100 people
+popularMovies=movieStats['rating']['size']>=100
+#ascending=false for mean
+movieStats[popularMovies].sort_values([('rating','mean')], ascending=False)[:15]
+df = movieStats[popularMovies].join(pd.DataFrame(similarMovies,columns=['similarity']))
+df.head()
+df.sort_values(['similarity'],ascending=False)[:5]
